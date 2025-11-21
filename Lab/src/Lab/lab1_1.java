44c25a405efc3912a42ce095451dcbfd6bba2101
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-11-21 13:29:18 +0700

    Lab1

--- Lab/src/Lab/lab1_1.java
+++ Lab/src/Lab/lab1_1.java
@@ -1,5 +1,13 @@
 package Lab;
-
+import java.util.Scanner;
 public class lab1_1 {
+	public static void main(String[] args) {
+      
+        Scanner input = new Scanner(System.in);
+       int number1 = input.nextInt();
+       int number2 = input.nextInt();
+        System.out.println(number1 + number2);
+        input.close();
+    }
 
 }
