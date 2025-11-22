d7ab45a4471ff16a8993f89916d3c1344a267f3a
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-11-22 22:35:15 +0700

    Lab1

--- Lab/src/Lab/Lab1_12.java
+++ Lab/src/Lab/Lab1_12.java
@@ -1,9 +1,39 @@
 package Lab;
 
+import java.util.Scanner;
+
 public class Lab1_12 {
 	public static void main(String[] args) {
-		
+        Scanner input = new Scanner(System.in);
 
-	}
+        System.out.print("Enter number of products: ");
+        int number = input.nextInt();
 
+        int[] id = new int[number];
+        int[] stock = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            System.out.print("Enter Product id: ");
+            id[i] = input.nextInt();
+
+            System.out.print("Enter Stock quantity: ");
+            stock[i] = input.nextInt();
+        }
+
+        System.out.print("Enter Product ID to search: ");
+        int search = input.nextInt();
+
+        boolean found = false;
+        for (int i = 0; i < number; i++) {
+            if (id[i] == search) {
+                System.out.println("Stock = " + stock[i]);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Product " + search + " not found");
+        }
+    }
 }
--- Lab/src/Lab/Lab1_13.java
+++ Lab/src/Lab/Lab1_13.java
@@ -1,9 +1,32 @@
 package Lab;
 
+import java.util.Scanner;
+
 public class Lab1_13 {
-	public static void main(String[] args) {
-		
+		 public static void main(String[] args) {
+		        Scanner input = new Scanner(System.in);
 
-	}
+		        System.out.print(":");  
+		        int R = input.nextInt();
+		        int C = input.nextInt();
+		        
+		        int[][] grid = new int[R][C];
+		        int onlineCount = 0; 
+		        
+		        
+		        for (int i = 0; i < R; i++) {
+		          System.out.print("Input:");  
+		          for (int j = 0; j < C; j++) {
+		                grid[i][j] = input.nextInt(); 
+		                if (grid[i][j] == 1) {
+		                    onlineCount++; 
+		                }
+		            }
+		            
+		        }
 
-}
+		        System.out.println(onlineCount);
+
+		        input.close();
+		    }
+		}
