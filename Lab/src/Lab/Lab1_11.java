06267a7d1fedf301323800542a165c9d3a652f61
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-11-22 20:52:36 +0700

    Lab1

--- Lab/src/Lab/Lab1_11.java
+++ Lab/src/Lab/Lab1_11.java
@@ -1,8 +1,34 @@
 package Lab;
 
-public class Lab1_11 {
-	public static void main(String[] args) {
-		
+import java.util.Scanner;
 
+public class Lab1_11 {
+		public static int sumarray (int[] numbers) {
+			
+	        int sum = 0;
+	        
+	        for (int i = 0; i < numbers.length; i++) {
+	            sum += numbers[i];
+	        }
+	        return sum;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+
+	        System.out.print("Enter your number :");
+	        int number = input.nextInt();
+
+	        int[]array = new int[number];
+
+	        for (int i = 0; i < number; i++) {
+	            System.out.print("Enter your number Array :");
+	            array[i] = input.nextInt();
+	            
+	        }
+
+	        int sum = sumarray(array);
+
+	        System.out.print("number Array : " + sum);
+	    }
 	}
-}
