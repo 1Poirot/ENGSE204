65545060e750ceb3a2ffbb5d97ac9ca17d544a88
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-12-01 20:51:12 +0700

    Lab2

---
+++ Lab2/src/Lab2_1/Student.java
@@ -0,0 +1,33 @@
+package Lab2_1;
+
+import java.util.Scanner;
+
+public class Student {
+	String studentid;
+	String name;
+ 
+ Student(String studentid, String name) {
+     this.studentid = studentid;
+     this.name = name;
+ }
+ 
+
+
+ public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     
+     System.out.print("Enter student ID: ");
+     String id = input.nextLine();
+
+     System.out.print("Enter Name: ");
+     String name = input.nextLine();
+     
+     Student s = new Student(id, name);
+     
+     System.out.println("Student ID " + s.studentid);
+     System.out.println("Name " + s.name);
+     
+     
+     input.close();
+ }
+}
