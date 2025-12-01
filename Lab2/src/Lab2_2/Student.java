a4e9bae9a8c1270ff53394ee75b4b50528095efe
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-12-02 00:11:30 +0700

    Lab

--- Lab2/src/Lab2_2/Student.java
+++ Lab2/src/Lab2_2/Student.java
@@ -5,30 +5,30 @@ import java.util.Scanner;
 public class Student {
 	private String studentid;
 	private String name;
- 
- Student(String studentid, String name) {
-     this.studentid = studentid;
-     this.name = name;
- }
- 
- void displayInfo() {
-     System.out.println("Student ID: " + studentid);
-     System.out.println("Name: " + name);
- }
+	
+	Student(String studentid, String name) {
+		this.studentid = studentid;
+		this.name = name;
+	}
+	
+	void displayInfo() {
+		System.out.println("Student ID: " + studentid);
+		System.out.println("Name: " + name);
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+     
+		System.out.print("Enter student ID: ");
+		String id = input.nextLine();
 
- public static void main(String[] args) {
-     Scanner input = new Scanner(System.in);
+		System.out.print("Enter Name: ");
+		String name = input.nextLine();
      
-     System.out.print("Enter student ID: ");
-     String id = input.nextLine();
-
-     System.out.print("Enter Name: ");
-     String name = input.nextLine();
-     
-     Student s = new Student(id, name);
-     
-     s.displayInfo();
-     input.close();
-     
- 	}
+		Student s = new Student(id, name);
+		
+		s.displayInfo();
+		input.close();
+		
+	}
 }
--- Lab2/src/Lab2_3/Student.java
+++ Lab2/src/Lab2_3/Student.java
@@ -3,10 +3,12 @@ package Lab2_3;
 public class Student {
 		private String studentid;
 		private String name;
-	 
+		private static int studentcount = 0; 
+		
 	 Student(String studentid, String name) {
 	     this.studentid = studentid;
 	     this.name = name;
+	     studentcount++;
 	 }
 	}
 
