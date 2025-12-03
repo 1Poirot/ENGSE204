81bad6f0f166627d7ef5c6c64c4599e1d25ffbaa
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-12-03 20:38:27 +0700

    Lab2

--- Lab2/src/Lab2_4/Student.java
+++ Lab2/src/Lab2_4/Student.java
@@ -2,56 +2,51 @@ package Lab2_4;
 
 import java.util.Scanner;
 
-
 public class Student {
-	private String name;
-	private int midtermScore;
-	private int finalScore;
-	
-	Student(String name, int midtermScore, int finalScore) {
-		this.name = name;
-		this.midtermScore = midtermScore;
-		this.finalScore = finalScore;
-		
-	}
-	double calculateAverage() {
-		return (midtermScore + finalScore) / 2.0;
- }
- 
- void displaySummary() {
-	 double avg = calculateAverage();
-	 System.out.println("Name: " + name);
-	 System.out.printf("Average Score: %.2f\n", avg);
+    private String name;
+    private int midtermScore;
+    private int finalScore;
 
-	    if (avg >= 50.0) {
-	        System.out.println("Status: Pass");
-	    } else {
-	        System.out.println("Status: Fail");
-	        
-	    }
- }
+    public Student(String name, int midtermScore, int finalScore) {
+        this.name = name;
+        this.midtermScore = midtermScore;
+        this.finalScore = finalScore;
+    }
+    
+    public double calculateaverage() {
+        return (midtermScore + finalScore) / 2.0;
+    }
+    
+    public void displaySummary() {
+        double avg = calculateaverage();
+        System.out.println("Name: " + name);
+        System.out.printf("Average Score: %.2f\n", avg);
 
- public static void main(String[] args) {
-	 Scanner input = new Scanner(System.in);
-	 
-	 System.out.print("Enter Student Name: ");
-	 String name = input.nextLine();
-	 
-	 System.out.print("Enter Midterm Score: ");
-     int mid = input.nextInt();
-     
-     System.out.print("Enter Final Score: ");
-     int fin = input.nextInt();
+        if (avg >= 50.0) {
+            System.out.println("Status: Pass");
+        } else {
+            System.out.println("Status: Fail");
+        }
+    }
 
-     Student student = new Student(name, mid, fin);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-     System.out.println();
+        System.out.print("Enter Student Name: ");
+        String name = input.nextLine();
 
-     student.displaySummary();
+        System.out.print("Enter Midterm Score: ");
+        int mid = input.nextInt();
 
-     input.close();
-     
- }
- 
+        System.out.print("Enter Final Score: ");
+        int fin = input.nextInt();
+
+        Student student = new Student(name, mid, fin);
+
+        System.out.println();
+
+        student.displaySummary();
+
+        input.close();
+    }
 }
-
