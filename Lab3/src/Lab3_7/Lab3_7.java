5167b272637a8d847313985f67d9e7d64508636a
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-12-13 21:50:18 +0700

    Lab

--- Lab3/src/Lab3_7/Lab3_7.java
+++ Lab3/src/Lab3_7/Lab3_7.java
@@ -1,5 +1,43 @@
 package Lab3_7;
+import java.util.Scanner;
 
 public class Lab3_7 {
+    private String employeeId;
+    private String department;
 
+    public Lab3_7(String employeeId, String department) {
+        this.employeeId = employeeId;
+        this.department = department;
+    }
+    
+    public String getEmployeeId() {
+        return this.employeeId;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public void setDepartment(String newDepartment) {
+        this.department = newDepartment;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter ID: ");
+        String id = input.nextLine();
+        
+        System.out.print("Enter the department : ");
+        String dept = input.nextLine();
+        
+        System.out.print("Enter the new department : ");
+        String newdept = input.nextLine();
+
+        Lab3_7 emp = new Lab3_7(id, dept);
+        emp.setDepartment(newdept);
+
+        System.out.println("ID:"+emp.getEmployeeId());
+        System.out.println("new department:"+emp.getDepartment());
+    }
 }
