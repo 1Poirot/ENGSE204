067ff6696caee628bdbb0859d91bfed8b1e33781
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-12-26 23:34:31 +0700

    Lab

--- Lab5/src/Lab5_7/Lab5_7.java
+++ Lab5/src/Lab5_7/Lab5_7.java
@@ -1,14 +1,82 @@
 package Lab5_7;
 
-public class Lab5_7 {
+import java.util.Scanner;
 
-	public Lab5_7() {
-		// TODO Auto-generated constructor stub
-	}
+class Employee {
+    protected String name;
+    protected double baseSalary;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public Employee(String name, double baseSalary) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
 
-	}
-
+    public double calculatePay() {
+        return baseSalary;
+    }
 }
+
+class SalesEmployee extends Employee {
+    protected double commissionRate;
+
+    public SalesEmployee(String name, double baseSalary, double commissionRate) {
+        super(name, baseSalary);
+        this.commissionRate = commissionRate;
+    }
+
+    @Override
+    public double calculatePay() {
+        return baseSalary + (baseSalary * commissionRate);
+    }
+}
+
+class Manager extends Employee {
+    protected double fixedBonus;
+
+    public Manager(String name, double baseSalary, double fixedBonus) {
+        super(name, baseSalary);
+        this.fixedBonus = fixedBonus;
+    }
+
+    @Override
+    public double calculatePay() {
+        return baseSalary + fixedBonus;
+    }
+}
+
+class PayrollProcessor {
+    public static void process(Employee e) {
+        System.out.println(e.name + " total pay: " + e.calculatePay());
+    }
+}
+
+public class Lab5_7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Sales Employee name: ");
+        String salesName = input.next();
+
+        System.out.print("Enter Sales Employee base salary: ");
+        double salesSalary = input.nextDouble();
+
+        System.out.print("Enter commission rate: ");
+        double rate = input.nextDouble();
+
+        System.out.print("Enter Manager name: ");
+        String managerName = input.next();
+
+        System.out.print("Enter Manager base salary: ");
+        double managerSalary = input.nextDouble();
+
+        System.out.print("Enter fixed bonus: ");
+        double bonus = input.nextDouble();
+
+        Employee sales = new SalesEmployee(salesName, salesSalary, rate);
+        Employee manager = new Manager(managerName, managerSalary, bonus);
+
+        PayrollProcessor.process(sales);
+        PayrollProcessor.process(manager);
+    }
+}
+
