9955d111d9076d07317ba63edc55c6addda65b3d
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-12-26 22:59:38 +0700

    Lab

--- Lab5/src/Lab5_4/Lab5_4.java
+++ Lab5/src/Lab5_4/Lab5_4.java
@@ -1,5 +1,40 @@
 package Lab5_4;
 
-public class Lab5_4 {
+import java.util.Scanner;
 
+class Vehicle {
+    protected String color;
+
+    public Vehicle(String color) {
+        this.color = color;
+    }
 }
+
+class Car extends Vehicle {
+    protected int wheels;
+
+    public Car(String color, int wheels) {
+        super(color);
+        this.wheels = wheels;
+    }
+
+    public void displayDetails() {
+        System.out.println("Color: " + color + ", Wheels: " + wheels);
+    }
+}
+
+public class Lab5_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter color: ");
+        String color = sc.next();
+
+        System.out.print("Enter number wheels: ");
+        int wheels = sc.nextInt();
+
+        Car car = new Car(color, wheels);
+        car.displayDetails();
+    }
+}
+
