bfadf76ad50fb399ca160935a38ae9fe4879331c
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-12-26 22:53:29 +0700

    Merge brancLh 'main' of https://github.com/1Poirot/ENGSE204.git into main

--- Lab5/src/Lab5_2/Lab5_2.java
+++ Lab5/src/Lab5_2/Lab5_2.java
@@ -1,5 +1,45 @@
 package Lab5_2;
 
-public class Lab5_2 {
+import java.util.Scanner;
 
+class Animal {
+
+    protected String name;
+    protected int age;
+
+    public void makeSound() {
+        System.out.println("I am an animal.");
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+class Cat extends Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow!");
+    }
+}
+
+public class Lab5_2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter cat name: ");
+        String name = sc.next();
+
+        System.out.print("Enter cat age: ");
+        int age = sc.nextInt();
+
+        Cat cat = new Cat();
+        cat.name = name;
+        cat.age = age;
+
+        cat.displayInfo();
+        cat.makeSound();
+    }
 }
--- Lab5/src/Lab5_3/Lab5_3.java
+++ Lab5/src/Lab5_3/Lab5_3.java
@@ -1,10 +1,36 @@
 package Lab5_3;
 
+import java.util.Scanner;
+
+class Employee {
+
+    protected double salary;
+
+    public double calculateBonus() {
+        return salary * 0.10;
+    }
+}
+
+class Manager extends Employee {
+
+    @Override
+    public double calculateBonus() {
+        return super.calculateBonus() + (salary * 0.05);
+    }
+}
+
 public class Lab5_3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        System.out.print("Enter salary: ");
+        double salary = sc.nextDouble();
 
+        Manager manager = new Manager();
+        manager.salary = salary;
+
+        double bonus = manager.calculateBonus();
+        System.out.println(bonus);
+    }
 }
