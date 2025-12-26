74716eaf4d26c41fee7e60d3d20f10f154763634
natthanicha <natthanicha@LAPTOP-OOA5BD1F>
2025-12-26 23:04:13 +0700

    Lab

--- Lab5/src/Lab5_5/Lab5_5.java
+++ Lab5/src/Lab5_5/Lab5_5.java
@@ -1,10 +1,75 @@
 package Lab5_5;
 
+import java.util.Scanner;
+
+class Shape {
+
+    protected String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public double calculateArea() {
+        return 0.0;
+    }
+}
+
+class Circle extends Shape {
+
+    protected double radius;
+
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 3.14159 * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+
+    protected double width;
+    protected double height;
+
+    public Rectangle(String name, double width, double height) {
+        super(name);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
+
 public class Lab5_5 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Radius: ");
+        double radius = input.nextDouble();
+        Circle circle = new Circle("Circle", radius);
+
+        System.out.print("Width: ");
+        double width = input.nextDouble();
+
+        System.out.print("Height: ");
+        double height = input.nextDouble();
+        Rectangle rectangle = new Rectangle("Rectangle", width, height);
+
+        Shape[] shapes = { circle, rectangle };
+
+        for (Shape s : shapes) {
+            System.out.println(s.calculateArea());
+        }
+
+        input.close();
+    }
 }
